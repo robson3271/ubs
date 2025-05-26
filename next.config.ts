c99b3d52b59ea0b1c2f587dc89ff631fc8d5b145
robson3271 <robsonpereiraje@gmail.com>
2025-05-25 23:43:56 -0300

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,33 @@
+const isProd = process.env.NODE_ENV === 'production';
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: !isProd,
+  fallbacks: {
+    document: '/offline.html',
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = withPWA(nextConfig);
